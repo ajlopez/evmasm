a4ed9d87cd21462875feda8146677d567c2df234
ajlopez <webmaster@ajlopez.com>
2018-03-20 14:16:17 -0300

    Lexer get name token with spaces

--- test/lexers.js
+++ test/lexers.js
@@ -21,3 +21,15 @@ exports['get name token'] = function (test) {
 	test.equal(lexer.nextToken(), null);
 };
 
+exports['get name token with spaces'] = function (test) {
+	var lexer = lexers.lexer('  foo  ');
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'foo');
+	test.equal(token.type, TokenType.Name);
+	
+	test.equal(lexer.nextToken(), null);
+};
+
