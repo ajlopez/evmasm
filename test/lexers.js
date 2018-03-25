39f9c36239384075c5c00e39a65bd2fcdb9c2324
ajlopez <webmaster@ajlopez.com>
2018-03-25 13:38:18 -0300

    Get label in lexer

--- test/lexers.js
+++ test/lexers.js
@@ -21,6 +21,18 @@ exports['get name token'] = function (test) {
 	test.equal(lexer.nextToken(), null);
 };
 
+exports['get label token'] = function (test) {
+	var lexer = lexers.lexer('foo:');
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'foo');
+	test.equal(token.type, TokenType.Label);
+	
+	test.equal(lexer.nextToken(), null);
+};
+
 exports['get name token with spaces'] = function (test) {
 	var lexer = lexers.lexer('  foo  ');
 	
