b99e47382e5bfb5690a30df3948402d61080adf8
ajlopez <webmaster@ajlopez.com>
2018-03-25 10:10:09 -0300

    Compile additional arithmetic operators

--- test/compiler.js
+++ test/compiler.js
@@ -15,5 +15,14 @@ exports['compile arithmetic opcodes'] = function (test) {
 	test.equal(compiler.compile('mul'), '02');
 	test.equal(compiler.compile('sub'), '03');
 	test.equal(compiler.compile('div'), '04');
+
+	test.equal(compiler.compile('sdiv'), '05');
+	test.equal(compiler.compile('mod'),  '06');
+	test.equal(compiler.compile('smod'), '07');
+	test.equal(compiler.compile('addmod'), '08');
+	test.equal(compiler.compile('mulmod'), '09');
+	test.equal(compiler.compile('exp'),  '0a');
+	test.equal(compiler.compile('signextend'),  '0b');
 }
+
 
