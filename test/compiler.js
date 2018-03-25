19ab5b636bab6ec021ef2a5f25cfe49e2aaafa29
ajlopez <webmaster@ajlopez.com>
2018-03-25 10:01:34 -0300

    Compile opcode in uppercase and mixed case

--- test/compiler.js
+++ test/compiler.js
@@ -5,6 +5,11 @@ exports['compile stop opcode'] = function (test) {
 	test.equal(compiler.compile('stop'), '00');
 }
 
+exports['compile opcode in uppercase and mixed case'] = function (test) {
+	test.equal(compiler.compile('STOP'), '00');
+	test.equal(compiler.compile('Add'), '01');
+}
+
 exports['compile arithmetic opcodes'] = function (test) {
 	test.equal(compiler.compile('add'), '01');
 	test.equal(compiler.compile('mul'), '02');
