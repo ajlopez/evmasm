9a77dcd793cc05ff3c645b332d062f5643e3c7bb
ajlopez <webmaster@ajlopez.com>
2018-03-25 13:08:00 -0300

    Opcode parse with code and code size

--- test/parsers.js
+++ test/parsers.js
@@ -12,6 +12,19 @@ exports['parse name expression'] = function (test) {
 	test.equal(parser.parseExpression(), null);
 }
 
+exports['parse opcode expression'] = function (test) {
+	var parser = parsers.parser('mstore');
+	
+	var expr = parser.parseExpression();
+	
+	test.ok(expr);
+	test.equal(expr.name(), 'mstore');
+	test.equal(expr.codesize(), 1);
+	test.equal(expr.code(), '52');
+	
+	test.equal(parser.parseExpression(), null);
+}
+
 exports['parse hexadecimal integer expression'] = function (test) {
 	var parser = parsers.parser('0x2a');
 	
