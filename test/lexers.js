d26596999b487ea575d59ed00f62c4368900008d
ajlopez <webmaster@ajlopez.com>
2018-03-26 12:28:17 -0300

    Skip line comment

--- test/lexers.js
+++ test/lexers.js
@@ -139,6 +139,25 @@ exports['get two name tokens skipping comment with spaces and new line'] = funct
 	test.equal(lexer.nextToken(), null);
 };
 
+exports['get two name tokens skipping line comment'] = function (test) {
+	var lexer = lexers.lexer('foo// this is a line comment \nbar');
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'foo');
+	test.equal(token.type, TokenType.Name);
+	
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'bar');
+	test.equal(token.type, TokenType.Name);
+
+	test.equal(lexer.nextToken(), null);
+};
+
 exports['get parenthesis as punctuation'] = function (test) {
 	var lexer = lexers.lexer('()');
 	
